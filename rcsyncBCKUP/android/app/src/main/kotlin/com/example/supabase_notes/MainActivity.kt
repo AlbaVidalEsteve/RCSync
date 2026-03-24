439ade32c08ff8e2a984684bcef45692663f5963
AlbaVidalEsteve <66486832+AlbaVidalEsteve@users.noreply.github.com>
2026-03-24 18:24:03 +0100

    Borro backup

--- rcsyncBCKUP/android/app/src/main/kotlin/com/example/supabase_notes/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.rcsync
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- rcsyncBCKUP/lib/app/modules/results/bindings/results_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import '../controllers/results_controller.dart';
-
-class ResultsBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut<ResultsController>(() => ResultsController());
-  }
-}
